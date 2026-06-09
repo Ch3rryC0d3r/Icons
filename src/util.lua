4d51a1cb1bf31cd2e68d40505206d0a0761de520
Breuhh <breuhh69@gmail.com>
2026-06-09 19:54:14 +0200

    Changed Icons.Icon to be SMODS.GameObject

--- src/util.lua
+++ src/util.lua
@@ -1,23 +1,26 @@
-function Icons.Icon(args)
-    if not args.key then sendErrorMessage("Missing key!","ICONS") return end
-    args.atlas = args.atlas or 'Joker'
-    args.pos = args.pos or {x = 0, y = 0}
-    args.targets = args.targets or {args.key}
+Icons.Icon = SMODS.GameObject:extend {
+    obj_table = Icons.Icons,
+    set = "Icons",
+    required_params = {
+        "key"
+    },
+
+    inject = function(self)
+        if not self.key then sendErrorMessage("Missing key!","ICONS") return end
+        self.atlas = self.atlas or 'Joker'
+        self.pos = self.pos or {x = 0, y = 0}
+        self.targets = self.targets or {self.key}
     
-    Icons.Icons[SMODS.current_mod.prefix..'_'..args.key] = {
-        key = SMODS.current_mod.prefix..'_'..args.key,
-        original_key = args.key,
-        atlas = args.atlas,
-        pos = args.pos,
-        targets = args.targets
-    }
-    sendDebugMessage(("Icon %s has been registered"):format(args.key),"ICONS")
-    --[[if not args.case_sensitive then
-        for i,v in pairs(args.target) do
-            args.target[i] = string.lower(v)
-        end
-    end]] --Probably remove this idk, just makes target lowercase if args.case_sensitive is false
-end
+        Icons.Icons[SMODS.current_mod.prefix..'_'..self.key] = {
+            key = SMODS.current_mod.prefix..'_'..self.key,
+            original_key = self.key,
+            atlas = self.atlas,
+            pos = self.pos,
+            targets = self.targets
+        }
+        sendDebugMessage(("Icon %s has been registered"):format(self.key),"ICONS")
+    end
+}
 
 -- misc_functions.lua has localize(), init_localization(), loc_parse_string()
 -- game.lua has G.localization line 993
