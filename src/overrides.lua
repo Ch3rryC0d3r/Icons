5dead20aaef0805a4c93d8505c2b7f11be286b77
Jogla <Joglacraft@gmail.com>
2026-06-10 12:46:17 +0200

    multi-line icon fix

--- src/overrides.lua
+++ src/overrides.lua
@@ -1,20 +1,26 @@
 local ref = loc_parse_string
 function loc_parse_string(...)
     local parsed_line = ref(...) or {}
-    local c = 1
+    G.Icons_temp_loc_acc = G.Icons_temp_loc_acc or 1
     for i=#parsed_line, 1, -1 do
-        if parsed_line[i].control and parsed_line[i].control.element then c = c + 1 end
+        if parsed_line[i].control and parsed_line[i].control.element then G.Icons_temp_loc_acc = G.Icons_temp_loc_acc + 1 end
         for _, v in pairs(Icons.Icons) do
             for _, vv in ipairs(v.targets) do
                 if parsed_line[i] and parsed_line[i].strings[1] == vv then
-                    table.insert(parsed_line,i,{strings = {}, control = {element = tostring(c)}})
+                    table.insert(parsed_line,i,{strings = {}, control = {element = tostring(G.Icons_temp_loc_acc)}})
                     i = i - 1
-                    c = c + 1
+                    G.Icons_temp_loc_acc = G.Icons_temp_loc_acc + 1
                 end
             end
         end
     end
     return parsed_line
+end
+
+local ref = init_localization
+function init_localization(...)
+    local ret = ref(...)
+    G.Icons_temp_loc_acc = nil
 end
 
 local ref = localize
